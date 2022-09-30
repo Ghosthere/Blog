dae3e21e0ea2dbb0cacd1bb82c88603209f59fc8
Aiden <aiden_zhang@epam.com>
2022-09-30 18:00:25 +0800

    start

---
+++ src/main/java/com/blog/BlogApplication.java
@@ -0,0 +1,12 @@
+package com.blog;
+
+/**
+ * @PackageName: com.blog
+ * @ClassName: BlogApplication
+ * @Description:
+ * @Author: AidenZ
+ * @Time: 9/30/2022 5:59 PM
+ */
+
+public class BlogApplication {
+}
